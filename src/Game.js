99989e9d90193ed269398879e11014ca4b882739
Seungwoo321 <seungwoo321@gmail.com>
2021-06-24 15:46:17 +0900

    add logo

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,5 @@
 
 import Phaser from 'phaser'
-// import Preloader from './Preloader'
 class Game extends Phaser.Game {
 
     constructor(option) {
@@ -10,21 +9,6 @@ class Game extends Phaser.Game {
 
     boot() {
         super.boot();
-        this.music = {};
-    }
-
-    playMusic(difficulty) {
-        if (!this.currentMusic) {
-            this.currentMusic = this.music[difficulty];
-        }
-
-        if (!this.currentMusic.isPlaying) {
-            this.currentMusic.play();
-        } else {
-            this.currentMusic.fadeOut(1000);
-            this.currentMusic = this.music[difficulty];
-            this.currentMusic.fadeIn(1000, true);
-        }
     }
 }
 
