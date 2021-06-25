248ad87d174a40392744d29174c6fd2945570e1d
Seungwoo321 <seungwoo321@gmail.com>
2021-06-25 18:03:04 +0900

    update name and add json sample

--- src/PlayGameScene.js
+++ src/PlayGameScene.js
@@ -7,20 +7,17 @@ class PlayGameScene extends Phaser.Scene {
 
     preload () {
         this.load.bitmapFont('atari', 'fonts/atari-classic.png', './fonts/atari-classic.xml')
-
         this.load.image('play-screen', 'images/play-screen.png')
 
-        this.load.path = 'images/aws/'
-
-        this.load.image('authentication', 'authentication.png')
-        this.load.image('s3', 'bucket.png')
-        this.load.image('cloudwatch', 'cloudwatch.png')
-        this.load.image('rds', 'database.png')
-        this.load.image('elb', 'ELB.png')
-        this.load.image('IAM', 'IAM.png')
-        this.load.image('lambda', 'lambda.png')
-        this.load.image('ec2', 'server.png')
-        this.load.image('vpc', 'vpc.png')
+        this.load.path = 'images/action/'
+        this.load.image('security', 'security.png')
+        this.load.image('storage', 'storage.png')
+        this.load.image('monitor', 'monitor.png')
+        this.load.image('database', 'database.png')
+        this.load.image('loadbalancing', 'loadbalancing.png')
+        this.load.image('event', 'event.png')
+        this.load.image('server', 'server.png')
+        this.load.image('network', 'network.png')
         this.load.image('autoscaling', 'autoscaling.png')
     }
 
@@ -28,39 +25,35 @@ class PlayGameScene extends Phaser.Scene {
         this.add.sprite(400, 300, 'play-screen')
 
         // left
-        const ec2 = this.add.sprite(60, 70, 'ec2').setOrigin(0.5).setScale(0.08).setInteractive()
-        this.add.bitmapText(60, 110, 'atari', 'SERVER').setOrigin(0.5).setScale(0.17)
-        const rds = this.add.sprite(60, 170, 'rds').setOrigin(0.5).setScale(0.08).setInteractive()
-        this.add.bitmapText(60, 210, 'atari', 'DATABASE').setOrigin(0.5).setScale(0.17)
-        const autoscaling = this.add.sprite(60, 270, 'autoscaling').setOrigin(0.5).setScale(0.08).setInteractive()
-        this.add.bitmapText(60, 310, 'atari', 'SCALING').setOrigin(0.5).setScale(0.17)
-        const elb = this.add.sprite(60, 370, 'elb').setOrigin(0.5).setScale(0.08).setInteractive()
-        this.add.bitmapText(60, 410, 'atari', 'BALANCING').setOrigin(0.5).setScale(0.17)
+        const server = this.add.sprite(60, 100, 'server').setOrigin(0.5).setScale(0.08).setInteractive()
+        this.add.bitmapText(60, 140, 'atari', 'SERVER').setOrigin(0.5).setScale(0.17)
+        const database = this.add.sprite(60, 200, 'database').setOrigin(0.5).setScale(0.08).setInteractive()
+        this.add.bitmapText(60, 240, 'atari', 'DATABASE').setOrigin(0.5).setScale(0.17)
+        const security = this.add.sprite(60, 300, 'autoscaling').setOrigin(0.5).setScale(0.08).setInteractive()
+        this.add.bitmapText(60, 340, 'atari', 'SECURITY').setOrigin(0.5).setScale(0.17)
+        const autoscaling = this.add.sprite(60, 400, 'loadbalancing').setOrigin(0.5).setScale(0.08).setInteractive()
+        this.add.bitmapText(60, 440, 'atari', 'SCALING').setOrigin(0.5).setScale(0.17)
 
         // right
-        const cloudwatch = this.add.sprite(750, 70, 'cloudwatch').setOrigin(0.5).setScale(0.08).setInteractive()
-        this.add.bitmapText(750, 110, 'atari', 'MONITOR').setOrigin(0.5).setScale(0.17)
-        const vpc = this.add.sprite(750, 170, 'vpc').setOrigin(0.5).setScale(0.08).setInteractive()
-        this.add.bitmapText(750, 210, 'atari', 'NETWORK').setOrigin(0.5).setScale(0.17)
-        const s3 = this.add.sprite(750, 270, 's3').setOrigin(0.5).setScale(0.08).setInteractive()
-        this.add.bitmapText(750, 310, 'atari', 'STORAGE').setOrigin(0.5).setScale(0.17)
-        const lambda = this.add.sprite(750, 370, 'lambda').setOrigin(0.5).setScale(0.08).setInteractive()
-        this.add.bitmapText(750, 410, 'atari', 'EVENT').setOrigin(0.5).setScale(0.17)
+        const monitor = this.add.sprite(750, 100, 'monitor').setOrigin(0.5).setScale(0.08).setInteractive()
+        this.add.bitmapText(750, 140, 'atari', 'MONITOR').setOrigin(0.5).setScale(0.17)
+        const network = this.add.sprite(750, 200, 'network').setOrigin(0.5).setScale(0.08).setInteractive()
+        this.add.bitmapText(750, 240, 'atari', 'NETWORK').setOrigin(0.5).setScale(0.17)
+        const storage = this.add.sprite(750, 300, 'storage').setOrigin(0.5).setScale(0.08).setInteractive()
+        this.add.bitmapText(750, 340, 'atari', 'STORAGE').setOrigin(0.5).setScale(0.17)
+        const event = this.add.sprite(750, 400, 'event').setOrigin(0.5).setScale(0.08).setInteractive()
+        this.add.bitmapText(750, 440, 'atari', 'EVENT').setOrigin(0.5).setScale(0.17)
 
-
-        button.on('vpc', actionOnClick, this)
-
+        // button.on('vpc', actionOnClick, this)]
         // let button = game.add.button(100, 400, 'vpc', actionOnClick, this);
         // button.onInputOver.add(over, this);
         // button.onInputOut.add(out, this);
         // button.onInputUp.add(up, this);
-
     }
 
-}
-
-function actionOnClick () {
-    console.log('clicked');
+    actionOnClick () {
+        console.log('clicked')
+    }
 }
 
 export {
