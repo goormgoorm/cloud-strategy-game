0cfbdfa8dcf8563b5fbd091bd75cb9f866667d0f
Seungwoo321 <seungwoo321@gmail.com>
2021-06-27 21:58:56 +0900

    add first random event

--- src/RandomEvent.js
+++ src/RandomEvent.js
@@ -7,21 +7,36 @@ class RandomEvent extends Phaser.Scene {
     }
 
     preload () {
-        this.load.json('actions', 'actions.json')
+        this.load.json('events', 'events.json')
     }
 
     create () {
-        this.actions = this.cache.json.get('actions')
+        this.history = []
+        this.quests = this.cache.json.get('events')
+        this.max = this.quests.length
+        console.log(this.random())
+    }
 
-        console.log(this.actions)
+    getRandomInt (min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min)) + min
     }
 
     random () {
-        console.log('random')
+        const num = this.getRandomInt(0, this.max)
+        const randomEvent = this.quests[num]
+        this.quests.splice(num, 1)
+        this.history.push(randomEvent)
+        return randomEvent
     }
 
     display () {
         console.log(111)
+    }
+
+    getHistory () {
+        return this.history
     }
 }
 
